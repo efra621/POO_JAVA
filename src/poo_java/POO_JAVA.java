f803af231bd6e8f6fb6647e3c7206bc9d643fdaf
EfrainMorales <eframorales421@gmail.com>
2024-05-06 00:48:17 -0300

    Explicacion de la clase modelo en este caso Clase Cliente

--- src/poo_java/POO_JAVA.java
+++ src/poo_java/POO_JAVA.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package poo_java;
 
 /**
@@ -10,11 +7,8 @@ package poo_java;
  */
 public class POO_JAVA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
